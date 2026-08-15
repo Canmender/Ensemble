34f9a512cc95c9f3e11f6ec9aed984d1919886b5
Canmender <Canmender@users.noreply.github.com>
2026-08-15 15:20:38 +0800

    chore(mobile): 版本号 0.7.54→0.7.55 (versionCode 44)，接入品牌 logo

--- mobile/src/services/connection.ts
+++ mobile/src/services/connection.ts
@@ -212,7 +212,7 @@ class ConnectionService {
       name: await this.getDeviceName(),
       type: "mobile",
       os: "React Native",
-      appVersion: "0.7.54",
+      appVersion: "0.7.55",
       wsPort: 0,
       httpPort: 0,
       ip: "0.0.0.0",
